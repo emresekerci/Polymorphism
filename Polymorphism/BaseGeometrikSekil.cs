84e2493df73ed4fd6d36ee3fef6ea3f8dcbcc112
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-14 21:26:48 +0300

    Add project files.

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    // Base sınıf
+    public class BaseGeometrikSekil
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+
+        // Alan hesaplayan metot
+        public virtual int AlanHesapla(int genislik, int yukseklik)
+        {
+            return genislik * yukseklik;
+        }
+    }
+
+    // Kare sınıfı
+    public class Kare : BaseGeometrikSekil
+    {
+        public override int AlanHesapla(int genislik, int yukseklik)
+        {
+            return base.AlanHesapla(genislik, yukseklik);
+        }
+    }
+
+    // Dikdörtgen sınıfı
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public override int AlanHesapla(int genislik, int yukseklik)
+        {
+            return base.AlanHesapla(genislik, yukseklik);
+        }
+    }
+
+    // Dik üçgen sınıfı
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        public override int AlanHesapla(int genislik, int yukseklik)
+        {
+            return (genislik * yukseklik) / 2;
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using Polymorphism;
+
+// Kare örneği
+BaseGeometrikSekil kare = new Kare();
+kare.Genislik = 5;
+kare.Yukseklik = 5;
+Console.WriteLine("Kare alanı: " + kare.AlanHesapla(kare.Genislik, kare.Yukseklik));
+
+// Dikdörtgen örneği
+BaseGeometrikSekil dikdortgen = new Dikdortgen();
+dikdortgen.Genislik = 5;
+dikdortgen.Yukseklik = 10;
+Console.WriteLine("Dikdörtgen alanı: " + dikdortgen.AlanHesapla(dikdortgen.Genislik, dikdortgen.Yukseklik));
+
+// Dik üçgen örneği
+BaseGeometrikSekil dikucgen = new DikUcgen();
+dikucgen.Genislik = 5;
+dikucgen.Yukseklik = 10;
+Console.WriteLine("Dik üçgen alanı: " + dikucgen.AlanHesapla(dikucgen.Genislik, dikucgen.Yukseklik));
